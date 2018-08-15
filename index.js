c9f8fbc603be24a50dda34d996f31b8ed561dde4
Job Rapati <jrapati@hotmail.nl>
2018-08-15 09:15:10 +0200

    updates!

--- index.js
+++ index.js
@@ -1,101 +1,221 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const axios = require('axios');
-const NodeCache = require("node-cache");
-const GameCache = new NodeCache();
+const fs = require('fs');
 
 let alreadyChosen = ["mario", "sonic"];
 const baseurl = "https://www.speedrun.com/api/v1";
 const platformids = ["8gejmne3" /*Amiga*/, "vm9vn63k" /*Arcade*/, "o0644863" /*Atari 2600*/, "gz9qox60" /*Commodore 64*/, "v06d394z" /*Dreamcast*/, "mr6k409z" /*Famicom Disk System*/, "gz9qkx90" /*Game & Watch*/, "n5683oev"/*Game Boy*/, "3167d6q2" /*Game Boy Advance*/, "gde3g9k1" /*Game Boy Color*/, "w89rwelk" /*Nintendo 64*/, "7g6m8erk" /*Nintendo DS*/, "jm95z9ol" /*Nintendo Entertainment System*/, "wxeod9rn" /*Playstation 1*/, "5negk9y7" /*Playstation Portable*/, "31670d9q" /*Sega CD*/, "w89r3w9l" /*Sega Game Gear*/, "mr6k0ezw" /*Sega Genesis*/, "83exwk6l" /*Sega Master System*/, "lq60l642" /*Sega Saturn*/, "83exk6l5" /*Super Nintendo Entertainment System*/, "n568zo6v" /*ZX Spectrum*/];
+const gametypeids = ["53no817x" /*Category Extension*/, "x3n6vme4" /*Prerelease*/, "j8138myw" /*Expansion/DLC*/];
 
-GameCache.set("test", "coolboy", 1000000000);
 let platformOffset = 0;
 
-function setGames()
-{
-    let games = [];
-    for(let i = 0; i < platformids.length; i++)
-    {
-        games[i] = [];
-        axios.get(baseurl + "/games?_bulk=yes&max=1000&platform=" + platformids[i])
-        .then(function(json) {
-        for(let j = 0; j < json.data.data.length; j++)
-        {
-            // console.log(json.data.data[j].names.international);
-            games[i][j] = json.data.data[j].names.international;
-        }
-
-        if(GameCache.get("games") === undefined || GameCache.get("games").length === undefined || GameCache.get("games").length === 0)
-                GameCache.set("games", games);
-        else
-            GameCache.set("games", GameCache.get("games").concat(games));
-        })
-        .catch(function(error)
-        {
-            console.log(error);
-        });
-        
-        if(i === platformOffset + 4)
-        {
-            return;
-        }
-    }
-    platformOffset = 5;
-}
-
-setGames();
-let timer = setInterval(setGames, 30000);
-if(platformOffset >= platformids.length)
-    clearInterval(timer);
-
-if(GameCache.get("picked") === undefined)
-    GameCache.set("picked", [], 1000000000);
-
 client.on('ready', () => {
-    // console.log(GameCache.get("test"));
+
 })
 
 client.on('message', msg => {
-    //Checks if someone uses the command and is mod
+
     if(msg.content === '!roll') 
     {
-        const randomnizedGame = "";
-        let alreadyChosen = GameCache.get("picked");
-        let games = GameCache.get("games");
+        //const randomnizedGame = "";
+        // let alreadyChosen = GameCache.get("picked");
+        let gamestext = fs.readFileSync('./games.txt').toString('utf-8');
+        let games = gamestext.split("\r\n");
         roll(msg, games);
-        //Checks if the game was already chosen the previous time
-        // for(let i = 0; i < alreadyChosen.length; i++) 
-        // {
-            // if(randomnizedGame == alreadyChosen[i]) 
-            // {
-            //     //reroll
-            // }
-            // else 
-            // {
-                // console.log(games[0]);
-                // let chosenGame = roll();
-                // if(chosenGame !== "")
-                // {
-                //     alreadyChosen.push(chosenGame);
-                //     msg.reply(chosenGame); 
-                // }
-
-            // }
-        // }
     }
 });
 
 function roll(msg, games)
 {
     let platform = Math.floor(Math.random() * games.length);
-    const chosenGame = games[platform][Math.floor(Math.random() * games[platform].length)];
-    if(chosenGame === undefined)
-    {
-        roll(msg, games);
-        return;
-    }
-    // console.log(chosenGame);
-    msg.reply(chosenGame);
+    const chosenGame = games[Math.floor(Math.random() * games.length)];
+    axios.get(baseurl + '/games/' + chosenGame).then((json) => {
+        let name = json.data.data.names.international;
+        let alreadyPlayed = false;
+        let played = fs.readFileSync('./alreadyplayed.txt').toString('utf-8').split("\r\n");
+        for(let i = 0; i < played.length; i++)
+        {
+            if(played[i] === name)
+                alreadyPlayed = true;
+        }
+        if(name === undefined || json.data.data.ruleset["emulators-allowed"] === false || alreadyPlayed)
+        {
+            roll(msg, games);
+            return;
+        }
+
+        let platforms = "";
+        for(let i = 0; i < json.data.data.platforms.length; i++)
+        {
+            let comma = true;
+            switch(json.data.data.platforms[i])
+            {
+                case "8gejmne3":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "Amiga";
+                    break;
+                case "vm9vn63k":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "Arcade";
+                    break;
+                case "o0644863":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "Atari 2600";
+                    break;
+                case "gz9qox60":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "Commodore 64";
+                    break;
+                case "v06d394z":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "Dreamcast";
+                    break;
+                case "mr6k409z":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "Famicom Disk System";
+                    break;
+                case "gz9qkx90":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "Game & Watch";
+                    break;
+                case "n5683oev":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "Game Boy";
+                    break;
+                case "3167d6q2":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "Game Boy Advance";
+                    break;
+                case "gde3g9k1":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "Game Boy Color";
+                    break;
+                case "w89rwelk":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "Nintendo 64";
+                    break;
+                case "7g6m8erk":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "Nintendo DS";
+                    break;
+                case "jm95z9ol":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "NES";
+                    break;
+                case "wxeod9rn":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "Playstation 1";
+                    break;                                        
+                case "5negk9y7":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "Playstation Portable";
+                    break;
+                case "31670d9q":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "Sega CD";
+                    break;                                
+                case "w89r3w9l":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "Sega Game Gear";
+                    break;
+                case "mr6k0ezw":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "Sega Genesis";
+                    break;
+                case "83exwk6l":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "Sega Master System";
+                    break;
+                case "lq60l642":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "Sega Saturn";
+                    break;
+                case "83exk6l5":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "SNES";
+                    break;
+                case "n568zo6v":
+                    if(i !== 0 && platforms !== "")
+                        platforms += ", "
+                    platforms += "ZX Spectrum";
+                    break;
+                default:
+                    comma = false;
+                    break;
+            }
+
+            // if(i < json.data.data.platforms.length - 1 && comma)
+            //     platforms += ", ";
+        }
+
+        let date = new Date();
+        let year = date.getFullYear();
+        let month = date.getMonth() + 1;
+        let day = date.getDate();
+        let startDate = year + "-" + month + "-" + day;
+        let newDay = date.getDate() + 7;
+        let endDate = new Date();
+        endDate.setDate(newDay);
+        let endYear = endDate.getFullYear();
+        let endMonth = endDate.getMonth() + 1;
+        let endDay = endDate.getDate();
+        let endDateString = endYear + "-" + endMonth + "-" + endDay;
+        
+        msg.channel.send({embed: {
+            color: 10980551,
+            title: name,
+            "image": {
+                "url": json.data.data.assets["cover-medium"].uri
+            },
+            fields: [
+                {
+                    name: "Week",
+                    value: startDate + " till " + endDateString
+                },
+                {
+                    name: "Release date",
+                    value: json.data.data.released
+                }, 
+                {
+                    name: "Platform",
+                    value: platforms
+                },
+                {
+                    name: "Speedrun.com link",
+                    value: json.data.data.weblink                                                
+                },
+            ],
+
+            footer: {
+            icon_url: 'https://cdn.discordapp.com/avatars/478586169865535488/7ac94aaf8d5e7456f8bda2f0629c06cf.png?size=128',
+            text: "Bot made by Job Rapati and Seydie."
+            }
+        }});
+
+        fs.writeFileSync('./alreadyplayed.txt', name);
+    });
 }
 
 client.login('NDc4NTg2MTY5ODY1NTM1NDg4.DlM4GQ.HXSrnqESttDNoyeCEwtpQSvRWNo');
